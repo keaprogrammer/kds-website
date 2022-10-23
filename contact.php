25258fbe2be1ff1bfc8ba60f66ab54e8b441f56e
keaprogrammer <Keaboka.m@protonmail.com>
2022-10-23 16:44:22 +0200

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,17 +1,21 @@
 <?php
 
-if (isset($_POST['volunteer-submit'])) {
+if (!empty($_POST['Submit'])) {
   $name = $_POST['volunteer-name'];
   $subject = $_POST['volunteer-subject'];
   $mailFrom = $_POST['volunteer-email'];
-  $message = $_POST['message'];
-
+  $message = $_POST['volunteer-message'];
   $mailTo = "info@keadigitalsolutions.co.za";
-  $headers = "From: ".$mailFrom;
-  $txt = "You have recieved an email from ".$name.".\n\n".$message;
 
-  mail($mailTo, $subject, $txt, $headers);
-  header("Location: index.html?mailsend");
+  $headers = "Name: ". $name .
+  "\r\n Email: " . $mailFrom .
+  "\r\n Subject: " . $subject .
+  "\r\n message: " . $message . "\r\n";
+
+  if(mail($mailTo, $name, $headers)){
+    $message = "Your information is Received Successfully."
+  }
+
 }
 
 ?>
